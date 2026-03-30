6373d6d0623df45f970055706c999395761a198a
Josema <josemaleoncarrera@gmail.com>
2026-03-30 07:22:04 -0500

    Nuevos Avances con galeria y efectos, agregar el footer (pie de pagina)

--- scripts/script_galeria.js
+++ scripts/script_galeria.js
@@ -1,31 +1,12 @@
-const lenis = new Lenis();
-lenis.on("scroll", ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-gsap.ticker.lagSmoothing(0);
+const links = document.querySelectorAll(".galeria-scroll nav a");
 
-gsap.registerPlugin(ScrollTrigger);
+links.forEach(link => {
+    link.addEventListener("click", () => {
 
-const reverseTrigger = gsap.utils.toArray(
-  ".col-scroll__box:nth-child(odd) .col-scroll__list"
-);
+        // quitar activo anterior
+        links.forEach(l => l.classList.remove("activo"));
 
-reverseTrigger.forEach((element) => {
-  const elementHeight = element.offsetHeight;
-  const viewportHeight = window.innerHeight;
-  const extraSpace = viewportHeight * 0.2;
-  const scrollDistance = elementHeight + viewportHeight + extraSpace;
-
-  gsap.to(element, {
-    yPercent: 100,
-    scrollTrigger: {
-      trigger: element,
-      start: 0,
-      end: `+=${scrollDistance}`,
-      scrub: true,
-      pin: true
-      // markers: true
-    }
-  });
+        // agregar activo actual
+        link.classList.add("activo");
+    });
 });
